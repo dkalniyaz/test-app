92ccd890abb13be8a35846a383733eebc887dc03
dkalniyaz <127355059+dkalniyaz@users.noreply.github.com>
2023-06-02 12:50:29 -0400

    2

--- src/components/BooksList/BookDetails/BookDetails.js
+++ src/components/BooksList/BookDetails/BookDetails.js
@@ -1,7 +1,9 @@
 import { useState, useEffect } from "react"
+import parse from 'html-react-parser'
 
 function BookDetails({bookId}){
     const [book, setBook] = useState(null)
+    const [error, setError] = useState(false)
 
     useEffect(()=>{
         fetch(`https://www.googleapis.com/books/v1/volumes/${bookId}`)
@@ -16,11 +18,15 @@ function BookDetails({bookId}){
         return <div>Loading...</div>
     }
 
+    if(error){
+        return <div className="book-error">Book not found.</div> 
+    }
+
     return (
         <div className="book">
             <h1>{book.volumeInfo.title}</h1>
             <h2>{book.volumeInfo.authors.join(', ')}</h2>
-            <p>{book.volumeInfo.description}</p>
+            <p>{book.volumeInfo.description ? parse(book.volumeInfo.description): 'No Description'}</p>
             {book.volumeInfo.imageLinks && <img src={book.volumeInfo.imageLinks.thumbnail} alt={book.volumeInfo.title} />}
         </div>
     )
